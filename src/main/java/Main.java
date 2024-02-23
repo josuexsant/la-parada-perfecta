3d56615792944f20de371fb8926b60688e55ae38
Josue Santamaria <josuexsanta@gmail.com>
2024-02-23 17:42:12 -0600

    Primer commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import model.CreateConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            CreateConnection createConn = new CreateConnection();
+            Connection conn = createConn.getConnection();
+            String query = "show tables";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String tableName = rs.getString(1); // El nombre de la tabla es el primer valor en cada fila
+                System.out.println(tableName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/model/CreateConnection.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+public class CreateConnection {
+    static Properties props = new Properties();
+    String hostname = null;
+    String port = null;
+    String database = null;
+    String username = null;
+    String password = null;
+
+    public CreateConnection() {
+        try (InputStream in = Files.newInputStream(FileSystems.getDefault().getPath(System.getProperty("user.dir") +
+                File.separator + "src/main/db_props.properties.properties"))) {
+            props.load(in);
+            in.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        loadProperties();
+    }
+
+    public final void loadProperties() {
+        this.hostname = props.getProperty("host_name");
+        port = props.getProperty("port_number");
+        database = props.getProperty("db_name");
+        username = props.getProperty("username");
+        password = props.getProperty("password");
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+        String jdbcUrl;
+        jdbcUrl = "jdbc:mysql://" + this.hostname + ":" +
+                this.port + "/" + this.database;
+        System.out.println(jdbcUrl);
+        conn = DriverManager.getConnection(jdbcUrl, this.username,
+                this.password);
+        System.out.println("    Successfully connected");
+        return conn;
+    }
+}
